d008402162e6a9b029b403528dea27e09186cbc1
epent <pentium95@mail.ru>
2021-08-03 15:51:41 +0300

    add Accounts to Home

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,14 @@ import Expenses from './Expenses';
 import Income from './Income';
 import Transfers from './Transfers';
 import HistoryLog from './HistoryLog';
+import Accounts from './Accounts';
 
 import Box from '@material-ui/core/Box';
 
 const Home = (props) => {
     return (
         <Box>
+            <Accounts />
             <Expenses expenseForm={props.expenseForm} expenseFormSubmitHandler={props.expenseFormSubmitHandler} setExpenseForm={props.setExpenseForm} />
             <Income incomeForm={props.incomeForm} inputFormSubmitHandler={props.inputFormSubmitHandler} setIncomeForm={props.setIncomeForm} />
             <Transfers transferForm={props.transferForm} transferFormSubmitHandler={props.transferFormSubmitHandler} setTransferForm={props.setTransferForm} />
