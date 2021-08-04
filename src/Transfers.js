91d3b4093e065c205519d14ba79e004e13593d66
epent <pentium95@mail.ru>
2021-08-04 15:48:23 +0300

    add deleting transfer transactions

--- src/Transfers.js
+++ src/Transfers.js
@@ -12,6 +12,8 @@ const Transfers = (props) => {
     loading: true,
   });
 
+  const fetchedAccountList = [];
+
   useEffect(() => {
     fetch(
       "https://expense-tracker-fd99a-default-rtdb.firebaseio.com/transfers.json"
@@ -42,6 +44,90 @@ const Transfers = (props) => {
     });
   };
 
+  const deleteTransferHandler = (
+    transferId,
+    transferAmount,
+    transferFrom,
+    transferTo
+  ) => {
+    const updatedTransferLog = transferLog.transferList.filter(
+      (expense) => expense.id !== transferId
+    );
+
+    setTransferLog({
+      ...transferLog,
+      transferList: updatedTransferLog,
+    });
+
+    // delete expense from db
+    fetch(
+      "https://expense-tracker-fd99a-default-rtdb.firebaseio.com/transfers/" +
+        transferId +
+        ".json",
+      {
+        method: "DELETE",
+      }
+    );
+
+    // fetch accountList from server
+    fetch(
+      "https://expense-tracker-fd99a-default-rtdb.firebaseio.com/accounts.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        for (let key in data) {
+          fetchedAccountList.push({
+            ...data[key],
+            id: key,
+          });
+        }
+      })
+
+      // update accountBalanceFrom after deleting transfer
+      .then((response) => {
+        const account = fetchedAccountList.filter(
+          (account) => account.Name === transferTo
+        );
+        const updatedAccount = {
+          Balance: Number(account[0].Balance) - Number(transferAmount),
+        };
+        const accountId = account[0].id;
+
+        // post changed accountBalance to server
+        fetch(
+          "https://expense-tracker-fd99a-default-rtdb.firebaseio.com/accounts/" +
+            accountId +
+            ".json",
+          {
+            method: "PATCH",
+            body: JSON.stringify(updatedAccount),
+          }
+        );
+      })
+
+      // update accountBalanceTo after deleting transfer
+      .then((response) => {
+        const account = fetchedAccountList.filter(
+          (account) => account.Name === transferFrom
+        );
+        const updatedAccount = {
+          Balance: Number(account[0].Balance) + Number(transferAmount),
+        };
+        const accountId = account[0].id;
+
+        // post changed accountBalance to server
+        fetch(
+          "https://expense-tracker-fd99a-default-rtdb.firebaseio.com/accounts/" +
+            accountId +
+            ".json",
+          {
+            method: "PATCH",
+            body: JSON.stringify(updatedAccount),
+          }
+        );
+      });
+  };
+
   return (
     <Box>
       <Typography variant="h3" gutterBottom color="textSecondary">
@@ -59,6 +145,7 @@ const Transfers = (props) => {
         transactionColor="textSecondary"
         amountColor="textSecondary"
         sign=""
+        deleteTransaction={deleteTransferHandler}
       />
     </Box>
   );
