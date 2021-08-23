c317618cc95eed1138fbd04feca4110f10869c57
epent <pentium95@mail.ru>
2021-08-23 16:25:41 +0300

    Balance cosmetic fix

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -3,17 +3,30 @@ import React from "react";
 import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    width: 250,
+    height: 85,
+    backgroundColor: "#fafafa",
+  },
+});
 
 const Balance = (props) => {
+  const classes = useStyles();
+
   return (
-    <Box>
-      <Paper elevation={3}>
-        <Typography variant="h5" gutterBottom color="textSecondary">
-          {`Total ${props.title}`}
-        </Typography>
-        <Typography variant="h5" gutterBottom color={props.amountColor}>
-          {`${props.amount} ILS`}
-        </Typography>
+    <Box my={3}>
+      <Paper elevation={3} className={classes.root}>
+        <Box p={1}>
+          <Typography variant="h5" gutterBottom color="textSecondary">
+            {`Total ${props.title}`}
+          </Typography>
+          <Typography variant="h5" gutterBottom color={props.amountColor}>
+            {`${props.amount} ILS`}
+          </Typography>
+        </Box>
       </Paper>
     </Box>
   );
