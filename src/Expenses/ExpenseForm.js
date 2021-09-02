c05eddd34233cc00139e0f493cdbedf7c02cd94f
epent <pentium95@mail.ru>
2021-09-02 16:35:12 +0300

    everything updates correctly after edit expense

--- src/Expenses/ExpenseForm.js
+++ src/Expenses/ExpenseForm.js
@@ -73,8 +73,8 @@ const ExpenseForm = (props) => {
   };
 
   // shared between both handlers - post changes in accounts/categories to db
-  const postChangedBalance = (type, id, updated) => {
-    fetch(
+  const postChangedBalance = async (type, id, updated) => {
+    await fetch(
       `https://expense-tracker-fd99a-default-rtdb.firebaseio.com/${type}/${id}.json`,
       {
         method: "PATCH",
@@ -199,153 +199,222 @@ const ExpenseForm = (props) => {
     };
     postEditedExpenseToDB();
 
-    if (props.editedExpenseForm.Amount !== expenseForm.Amount) {
-      const updateAccountBalance = async () => {
-        const fetchedAccountList = await fetchDataToList("accounts");
+    const updateData = async () => {
+      const updateAmount = async () => {
+        if (props.editedExpenseForm.Amount !== expenseForm.Amount) {
+          const updateAccountBalance = async () => {
+            const fetchedAccountList = await fetchDataToList("accounts");
 
-        const updateBalanceInDB = () => {
-          const account = fetchedAccountList.filter(
-            (account) => account.Name === expenseForm.From
-          );
-          let updatedAccount;
-          let accountId;
+            const updateBalanceInDB = () => {
+              const account = fetchedAccountList.filter(
+                (account) => account.Name === props.editedExpenseForm.From
+              );
+              let updatedAccount;
+              let accountId;
 
-          if (props.editedExpenseForm.Amount > expenseForm.Amount) {
-            updatedAccount = {
-              Balance:
-                Number(account[0].Balance) +
-                (Number(props.editedExpenseForm.Amount) -
-                  Number(expenseForm.Amount)),
+              if (props.editedExpenseForm.Amount > expenseForm.Amount) {
+                updatedAccount = {
+                  Balance:
+                    Number(account[0].Balance) +
+                    (Number(props.editedExpenseForm.Amount) -
+                      Number(expenseForm.Amount)),
+                };
+                accountId = account[0].id;
+              }
+
+              if (props.editedExpenseForm.Amount < expenseForm.Amount) {
+                updatedAccount = {
+                  Balance:
+                    Number(account[0].Balance) -
+                    (Number(expenseForm.Amount) -
+                      Number(props.editedExpenseForm.Amount)),
+                };
+                accountId = account[0].id;
+              }
+              postChangedBalance("accounts", accountId, updatedAccount);
             };
-            accountId = account[0].id;
-          }
+            updateBalanceInDB();
+          };
+          await updateAccountBalance();
 
-          if (props.editedExpenseForm.Amount < expenseForm.Amount) {
-            updatedAccount = {
-              Balance:
-                Number(account[0].Balance) -
-                (Number(expenseForm.Amount) -
-                  Number(props.editedExpenseForm.Amount)),
+          const updateCategoryBalance = async () => {
+            const fetchedCategoryList = await fetchDataToList("categories");
+
+            const updateBalanceInDB = () => {
+              const category = fetchedCategoryList.filter(
+                (category) => category.Name === props.editedExpenseForm.To
+              );
+              let updatedCategory;
+              let categoryId;
+
+              if (props.editedExpenseForm.Amount > expenseForm.Amount) {
+                updatedCategory = {
+                  Balance:
+                    Number(category[0].Balance) -
+                    (Number(props.editedExpenseForm.Amount) -
+                      Number(expenseForm.Amount)),
+                };
+                categoryId = category[0].id;
+              }
+
+              if (props.editedExpenseForm.Amount < expenseForm.Amount) {
+                updatedCategory = {
+                  Balance:
+                    Number(category[0].Balance) +
+                    (Number(expenseForm.Amount) -
+                      Number(props.editedExpenseForm.Amount)),
+                };
+                categoryId = category[0].id;
+              }
+              postChangedBalance("categories", categoryId, updatedCategory);
             };
-            accountId = account[0].id;
-          }
-          postChangedBalance("accounts", accountId, updatedAccount);
-        };
-        updateBalanceInDB();
-      };
-      updateAccountBalance();
+            updateBalanceInDB();
+          };
+          await updateCategoryBalance();
 
-      const updateCategoryBalance = async () => {
-        const fetchedCategoryList = await fetchDataToList("categories");
+          const updateTotalBalance = async () => {
+            const fetchedTotalList = await fetchDataToList("total", true);
 
-        const updateBalanceInDB = () => {
-          const category = fetchedCategoryList.filter(
-            (category) => category.Name === expenseForm.To
-          );
-          let updatedCategory;
-          let categoryId;
-
-          if (props.editedExpenseForm.Amount > expenseForm.Amount) {
-            updatedCategory = {
-              Balance:
-                Number(category[0].Balance) -
-                (Number(props.editedExpenseForm.Amount) -
-                  Number(expenseForm.Amount)),
-            };
-            categoryId = category[0].id;
-          }
-
-          if (props.editedExpenseForm.Amount < expenseForm.Amount) {
-            updatedCategory = {
-              Balance:
-                Number(category[0].Balance) +
-                (Number(expenseForm.Amount) -
-                  Number(props.editedExpenseForm.Amount)),
-            };
-            categoryId = category[0].id;
-          }
-          postChangedBalance("categories", categoryId, updatedCategory);
-        };
-        updateBalanceInDB();
-      };
-      updateCategoryBalance();
-
-      const updateTotalBalance = async () => {
-        const fetchedTotalList = await fetchDataToList("total", true);
-
-        const updateBalanceInDB = () => {
-          const totalExpenses = fetchedTotalList.filter((total) => {
-            return total.id === "expenses";
-          });
-
-          // const totalIncome = fetchedBalanceList.filter((total) => {
-          //   return total.id === "income";
-          // });
-
-          // const totalBalance = fetchedBalanceList.filter((total) => {
-          //   return total.id === "balance";
-          // });
-
-          let updatedTotals;
-
-          if (props.editedExpenseForm.Amount > expenseForm.Amount) {
-            updatedTotals = {
-              expenses:
-                Number(totalExpenses[0].expenses) -
-                (Number(props.editedExpenseForm.Amount) -
-                  Number(expenseForm.Amount)),
-            };
-          }
-
-          if (props.editedExpenseForm.Amount < expenseForm.Amount) {
-            updatedTotals = {
-              expenses:
-                Number(totalExpenses[0].expenses) +
-                (Number(expenseForm.Amount) -
-                  Number(props.editedExpenseForm.Amount)),
-            };
-          }
-
-          fetch(
-            "https://expense-tracker-fd99a-default-rtdb.firebaseio.com/total.json",
-            {
-              method: "PATCH",
-              body: JSON.stringify(updatedTotals),
-            }
-          )
-            .then((response) => {
-              setExpenseForm({
-                From: "",
-                To: "",
-                Amount: 0,
-                Date: new Date().toDateString(),
-                Comment: "",
+            const updateBalanceInDB = () => {
+              const totalExpenses = fetchedTotalList.filter((total) => {
+                return total.id === "expenses";
               });
-            })
 
-            // trigger the page to rerender with updated expenseLog
-            .then((response) => props.updateExpenseLog())
-            // trigger Home to rerender with updated accountLog/categoryLog
-            .then((response) => {
-              if (props.updateHomeHandler) props.updateHomeHandler();
-            });
-        };
-        updateBalanceInDB();
+              // const totalBalance = fetchedBalanceList.filter((total) => {
+              //   return total.id === "balance";
+              // });
+
+              let updatedTotals;
+
+              if (props.editedExpenseForm.Amount < expenseForm.Amount) {
+                updatedTotals = {
+                  expenses:
+                    Number(totalExpenses[0].expenses) -
+                    (Number(expenseForm.Amount) -
+                      Number(props.editedExpenseForm.Amount)),
+                };
+              }
+
+              if (props.editedExpenseForm.Amount > expenseForm.Amount) {
+                updatedTotals = {
+                  expenses:
+                    Number(totalExpenses[0].expenses) +
+                    (Number(props.editedExpenseForm.Amount) -
+                      Number(expenseForm.Amount)),
+                };
+              }
+
+              fetch(
+                "https://expense-tracker-fd99a-default-rtdb.firebaseio.com/total.json",
+                {
+                  method: "PATCH",
+                  body: JSON.stringify(updatedTotals),
+                }
+              );
+            };
+            updateBalanceInDB();
+          };
+          await updateTotalBalance();
+        }
       };
-      updateTotalBalance();
-    }
+      await updateAmount();
 
-    if (props.editedExpenseForm.From !== expenseForm.From) {
-    }
+      const updateAccount = async () => {
+        if (props.editedExpenseForm.From !== expenseForm.From) {
+          const updateAccountBalance = async (PrevOrCurr) => {
+            const fetchedAccountList = await fetchDataToList("accounts");
 
-    if (props.editedExpenseForm.To !== expenseForm.To) {
-    }
+            const updateBalanceInDB = async () => {
+              let accountName;
+              PrevOrCurr === "Previous"
+                ? (accountName = props.editedExpenseForm.From)
+                : (accountName = expenseForm.From);
 
-    if (props.editedExpenseForm.Date !== expenseForm.Date) {
-    }
+              const account = fetchedAccountList.filter(
+                (account) => account.Name === accountName
+              );
+              let updatedAccount;
 
-    if (props.editedExpenseForm.Comment !== expenseForm.Comment) {
-    }
+              PrevOrCurr === "Previous"
+                ? (updatedAccount = {
+                    Balance:
+                      Number(account[0].Balance) + Number(expenseForm.Amount),
+                  })
+                : (updatedAccount = {
+                    Balance:
+                      Number(account[0].Balance) - Number(expenseForm.Amount),
+                  });
+
+              const accountId = account[0].id;
+              await postChangedBalance("accounts", accountId, updatedAccount);
+            };
+            await updateBalanceInDB();
+          };
+          await updateAccountBalance("Previous");
+          await updateAccountBalance("Current");
+        }
+      };
+      await updateAccount();
+
+      const updateCategory = async () => {
+        if (props.editedExpenseForm.To !== expenseForm.To) {
+          const updateCategoryBalance = async (PrevOrCurr) => {
+            const fetchedCategoryList = await fetchDataToList("categories");
+
+            const updateBalanceInDB = async () => {
+              let categoryName;
+              PrevOrCurr === "Previous"
+                ? (categoryName = props.editedExpenseForm.To)
+                : (categoryName = expenseForm.To);
+
+              const category = fetchedCategoryList.filter(
+                (category) => category.Name === categoryName
+              );
+              let updatedCategory;
+
+              PrevOrCurr === "Previous"
+                ? (updatedCategory = {
+                    Balance:
+                      Number(category[0].Balance) - Number(expenseForm.Amount),
+                  })
+                : (updatedCategory = {
+                    Balance:
+                      Number(category[0].Balance) + Number(expenseForm.Amount),
+                  });
+
+              const categoryId = category[0].id;
+
+              await postChangedBalance(
+                "categories",
+                categoryId,
+                updatedCategory
+              );
+            };
+            await updateBalanceInDB();
+          };
+          await updateCategoryBalance("Previous");
+          await updateCategoryBalance("Current");
+        }
+      };
+      await updateCategory();
+
+      const triggerUpdates = async () => {
+        setExpenseForm({
+          From: "",
+          To: "",
+          Amount: 0,
+          Date: new Date().toDateString(),
+          Comment: "",
+        });
+
+        // trigger the page to rerender with updated expenseLog
+        await props.updateExpenseLog();
+        // trigger Home to rerender with updated accountLog/categoryLog
+        await props.updateHomeHandler();
+      };
+      await triggerUpdates();
+    };
+    updateData();
 
     // close the editable form automatically
     props.setShowExpenseForm();
