289c94d1b41d5f580f5089a0fa74e4be192729b6
epent <pentium95@mail.ru>
2021-09-27 13:21:23 +0300

    create TransactionList with DataGrid

---
+++ src/components/History/TransactionList.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import { DataGrid } from "@material-ui/data-grid";
+
+import { getDataFromDB } from "../../modules/fetch";
+
+const useStyles = makeStyles({
+  root: {
+    width: 830,
+    backgroundColor: "#fafafa",
+    borderRadius: 10,
+  },
+});
+
+const TransactionList = (props) => {
+  const classes = useStyles();
+
+  const [rows, setRows] = useState([
+    {
+      id: "",
+      date: "",
+      from: "",
+      to: "",
+      amount: "",
+    },
+  ]);
+
+  const pushFetchedDataToList = (data, transactionType) => {
+    const list = [];
+    transactionType === "expenses"
+      ? Object.keys(data).map((key) => {
+          list.push({
+            ...data[key],
+            id: key,
+            type: "expenses",
+          });
+        })
+      : transactionType === "income"
+      ? Object.keys(data).map((key) => {
+          list.push({
+            ...data[key],
+            id: key,
+            type: "income",
+          });
+        })
+      : Object.keys(data).map((key) => {
+          list.push({
+            ...data[key],
+            id: key,
+            type: "transfers",
+          });
+        });
+    return list;
+  };
+
+  const columns = [
+    { field: "date", headerName: "Date", type: "date", width: 200 },
+    { field: "from", headerName: "From", width: 200 },
+    { field: "to", headerName: "To", width: 200 },
+    { field: "amount", headerName: "Amount", type: "number", width: 200 },
+  ];
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const expenses = await getDataFromDB("expenses");
+      const income = await getDataFromDB("income");
+      const transfers = await getDataFromDB("transfers");
+
+      const expenseList = pushFetchedDataToList(expenses, "expenses");
+      const incomeList = pushFetchedDataToList(income, "income");
+      const transferList = pushFetchedDataToList(transfers, "transfers");
+
+      const updateRowList = async () => {
+        const transactionList = [
+          ...expenseList,
+          ...incomeList,
+          ...transferList,
+        ];
+        const rowList = [];
+
+        transactionList.map((transaction) => {
+          const [weekday, month, day, year] = transaction.Date.split(" ");
+
+          let sign;
+          transaction.type === "expenses"
+            ? (sign = "-")
+            : transaction.type === "income"
+            ? (sign = "+")
+            : (sign = "");
+
+          rowList.push({
+            id: transaction.id,
+            date: new Date(`${day} ${month} ${year}`),
+            from: transaction.From,
+            to: transaction.To,
+            amount: `${sign}${transaction.Amount} ILS`,
+          });
+        });
+
+        setRows(rowList);
+      };
+      await updateRowList();
+    };
+    fetchTransactions();
+  }, []);
+
+  return (
+    <Grid container>
+      <Paper elevation={3} className={classes.root}>
+        <Box mt={3} mx={3}>
+          <Typography variant="h5" gutterBottom color="textSecondary">
+            Transactions
+          </Typography>
+        </Box>
+        <Box style={{ width: 830 }}>
+          <DataGrid
+            autoHeight={true}
+            rowHeight={45}
+            columns={columns}
+            rows={rows}
+            pageSize={15}
+          />
+        </Box>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default TransactionList;
