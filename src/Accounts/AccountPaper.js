efec152ad5705ba54380963b151ec889722158b4
epent <pentium95@mail.ru>
2021-11-30 19:06:04 +0200

    move AccountList and AccountsDonut to AccountPaper

---
+++ src/Accounts/AccountPaper.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+import AccountList from "./AccountList";
+import NewAccountForm from "./NewAccountForm";
+import AccountsDonut from "../Charts/AccountsDonut";
+import { fetchAccountsFromDB } from "../modules/fetch";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    backgroundColor: "#fafafa",
+    borderRadius: 10,
+    [theme.breakpoints.up("md")]: {
+      height: 345,
+    },
+  },
+}));
+
+const AccountPaper = (props) => {
+  const classes = useStyles();
+
+  const [accountList, setAccountList] = useState([]);
+
+  useEffect(() => {
+    // fetch accountList from server when form is opened
+    const fetchAccounts = async () => {
+      const fetchedAccountList = await fetchAccountsFromDB();
+
+      setAccountList(fetchedAccountList);
+    };
+    fetchAccounts();
+  }, [props.updateAccounts, props.updateHome]);
+
+  return (
+    <Paper elevation={3} className={classes.paper}>
+      <Grid container>
+        <Grid item xs={12} sm={6}>
+          <AccountList
+            accountList={accountList}
+            sliceLog={props.sliceLog}
+            showEditBtn={props.showEditBtn}
+            showDeleteBtn={props.showDeleteBtn}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AccountsDonut
+            updateAccounts={props.updateAccounts}
+            updateHome={props.updateHome}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default AccountPaper;
--- src/Accounts/NewAccounts.js
+++ src/Accounts/NewAccounts.js
@@ -2,41 +2,12 @@ import React, { useState, useEffect } from "react";
 
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 
-import AccountList from "./AccountList";
 import NewAccountForm from "./NewAccountForm";
-import AccountsDonut from "../Charts/AccountsDonut";
-import { fetchAccountsFromDB } from "../modules/fetch";
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    backgroundColor: "#fafafa",
-    borderRadius: 10,
-    [theme.breakpoints.up("md")]: {
-      height: 345,
-    },
-  },
-}));
+import AccountPaper from "./AccountPaper";
 
 const NewAccounts = (props) => {
-  const classes = useStyles();
-
-  const [accountList, setAccountList] = useState([]);
-
   const [updateAccounts, setUpdateAccounts] = useState(false);
-
-  useEffect(() => {
-    // fetch accountList from server when form is opened
-    const fetchAccounts = async () => {
-      const fetchedAccountList = await fetchAccountsFromDB();
-
-      setAccountList(fetchedAccountList);
-    };
-    fetchAccounts();
-  }, [updateAccounts]);
 
   // update the list of accounts
   const updateAccountsHandler = () => {
@@ -48,25 +19,11 @@ const NewAccounts = (props) => {
       <Grid item xs={12} md={6}>
         <NewAccountForm
           pageTitle="Add new account"
-          updateAccounts={updateAccountsHandler}
+          updateAccountsHandler={updateAccountsHandler}
         />
       </Grid>
       <Grid item xs={12} md={6}>
-        <Paper elevation={3} className={classes.paper}>
-          <Grid container>
-            <Grid item xs={12} sm={6}>
-              <AccountList
-                accountList={accountList}
-                sliceLog={props.sliceLog}
-                showEditBtn={props.showEditBtn}
-                showDeleteBtn={props.showDeleteBtn}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <AccountsDonut updateAccounts={updateAccounts} />
-            </Grid>
-          </Grid>
-        </Paper>
+        <AccountPaper updateAccounts={updateAccounts} />
       </Grid>
     </Grid>
   );
