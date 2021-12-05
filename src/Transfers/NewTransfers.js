63abc338ea2a2d159a33be2e86ce735b417bc461
epent <pentium95@mail.ru>
2021-12-05 16:41:36 +0200

    add editHandler to Transfers

--- src/Transfers/NewTransfers.js
+++ src/Transfers/NewTransfers.js
@@ -9,9 +9,10 @@ import {
   postUpdatedBalance,
   deleteTransactionFromDB,
   fetchDataToList,
+  postEditedTransactionToDB,
 } from "../modules/fetch";
 
-const NewTransfers = () => {
+const NewTransfers = (props) => {
   const [updateTransfers, setUpdateTransfers] = useState(false);
 
   const updateTransfersHandler = () => {
@@ -80,6 +81,184 @@ const NewTransfers = () => {
       updateData(transferToDelete)
     );
   };
+
+  // const editRowsHandler = (row, oldRow) => {
+  //   let transferForm;
+
+  //   const updateTransaction = () => {
+  //     let id;
+  //     Object.keys(row).map((key) => {
+  //       id = key;
+
+  //       let amountString = row[key].amount.value;
+  //       console.log(typeof amountString);
+  //       let amountArray = amountString.toString().split("");
+  //       amountArray.shift();
+  //       let amount = amountArray.join("");
+
+  //       transferForm = {
+  //         Amount: amount,
+  //         From: row[key].from.value,
+  //         To: row[key].to.value,
+  //         Date: row[key].date.value.toDateString(),
+  //       };
+  //     });
+  //     console.log(transferForm);
+  //     postEditedTransactionToDB(transferForm, "transfers", id);
+  //   };
+  //   updateTransaction();
+
+  //   const updateData = async () => {
+  //     const updateAccountFrom = async () => {
+  //       if (oldRow.From !== transferForm.From) {
+  //         const updateAccountBalance = async (PrevOrCurr) => {
+  //           const fetchedAccountList = await fetchDataToList("accounts");
+
+  //           const updateBalanceInDB = async () => {
+  //             let accountName;
+  //             PrevOrCurr === "Previous"
+  //               ? (accountName = oldRow.From)
+  //               : (accountName = transferForm.From);
+
+  //             const account = fetchedAccountList.filter(
+  //               (account) => account.Name === accountName
+  //             );
+  //             let updatedAccount;
+
+  //             PrevOrCurr === "Previous"
+  //               ? (updatedAccount = {
+  //                   Balance: Number(account[0].Balance) + Number(oldRow.Amount),
+  //                 })
+  //               : (updatedAccount = {
+  //                   Balance: Number(account[0].Balance) - Number(oldRow.Amount),
+  //                 });
+
+  //             const accountId = account[0].id;
+  //             await postUpdatedBalance("accounts", accountId, updatedAccount);
+  //           };
+  //           await updateBalanceInDB();
+  //         };
+  //         await updateAccountBalance("Previous");
+  //         await updateAccountBalance("Current");
+  //       }
+  //     };
+  //     await updateAccountFrom();
+
+  //     const updateAccountTo = async () => {
+  //       if (oldRow.To !== transferForm.To) {
+  //         const updateAccountBalance = async (PrevOrCurr) => {
+  //           const fetchedAccountList = await fetchDataToList("accounts");
+
+  //           const updateBalanceInDB = async () => {
+  //             let accountName;
+  //             PrevOrCurr === "Previous"
+  //               ? (accountName = oldRow.To)
+  //               : (accountName = transferForm.To);
+
+  //             const account = fetchedAccountList.filter(
+  //               (account) => account.Name === accountName
+  //             );
+  //             let updatedAccount;
+
+  //             PrevOrCurr === "Previous"
+  //               ? (updatedAccount = {
+  //                   Balance: Number(account[0].Balance) - Number(oldRow.Amount),
+  //                 })
+  //               : (updatedAccount = {
+  //                   Balance: Number(account[0].Balance) + Number(oldRow.Amount),
+  //                 });
+
+  //             const accountId = account[0].id;
+  //             await postUpdatedBalance("accounts", accountId, updatedAccount);
+  //           };
+  //           await updateBalanceInDB();
+  //         };
+  //         await updateAccountBalance("Previous");
+  //         await updateAccountBalance("Current");
+  //       }
+  //     };
+  //     await updateAccountTo();
+
+  //     const updateAmount = async () => {
+  //       if (oldRow.Amount !== transferForm.Amount) {
+  //         const updateAccountBalanceFrom = async () => {
+  //           const fetchedAccountList = await fetchDataToList("accounts");
+
+  //           const updateBalanceInDB = async () => {
+  //             const account = fetchedAccountList.filter(
+  //               (account) => account.Name === transferForm.From
+  //             );
+
+  //             let updatedAccount;
+  //             let accountId;
+
+  //             if (oldRow.Amount > transferForm.Amount) {
+  //               updatedAccount = {
+  //                 Balance:
+  //                   Number(account[0].Balance) +
+  //                   (Number(oldRow.Amount) - Number(transferForm.Amount)),
+  //               };
+  //               accountId = account[0].id;
+  //             }
+
+  //             if (oldRow.Amount < transferForm.Amount) {
+  //               updatedAccount = {
+  //                 Balance:
+  //                   Number(account[0].Balance) -
+  //                   (Number(transferForm.Amount) - Number(oldRow.Amount)),
+  //               };
+  //               accountId = account[0].id;
+  //             }
+  //             await postUpdatedBalance("accounts", accountId, updatedAccount);
+  //           };
+  //           await updateBalanceInDB();
+  //         };
+  //         await updateAccountBalanceFrom();
+
+  //         const updateAccountBalanceTo = async () => {
+  //           const fetchedAccountList = await fetchDataToList("accounts");
+
+  //           const updateBalanceInDB = async () => {
+  //             const account = fetchedAccountList.filter(
+  //               (account) => account.Name === transferForm.To
+  //             );
+
+  //             let updatedAccount;
+  //             let accountId;
+
+  //             if (oldRow.Amount > transferForm.Amount) {
+  //               updatedAccount = {
+  //                 Balance:
+  //                   Number(account[0].Balance) -
+  //                   (Number(oldRow.Amount) - Number(transferForm.Amount)),
+  //               };
+  //               accountId = account[0].id;
+  //             }
+
+  //             if (oldRow.Amount < transferForm.Amount) {
+  //               updatedAccount = {
+  //                 Balance:
+  //                   Number(account[0].Balance) +
+  //                   (Number(transferForm.Amount) - Number(oldRow.Amount)),
+  //               };
+  //               accountId = account[0].id;
+  //             }
+  //             await postUpdatedBalance("accounts", accountId, updatedAccount);
+  //           };
+  //           await updateBalanceInDB();
+  //         };
+  //         await updateAccountBalanceTo();
+  //       }
+  //     };
+  //     await updateAmount();
+
+  //     const triggerPageUpdates = async () => {
+  //       setUpdateTransfers((prevState) => !prevState);
+  //     };
+  //     await triggerPageUpdates();
+  //   };
+  //   updateData();
+  // };
 
   return (
     <Box>
