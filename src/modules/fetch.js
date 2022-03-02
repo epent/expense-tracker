cea00015f572faef975e1da5190aa76daa7f1923
epent <pentium95@mail.ru>
2022-03-02 12:44:24 +0200

    remove redundant funcs from fetch

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -1,43 +1,8 @@
-const baseURL = "https://expense-tracker-fd99a-default-rtdb.firebaseio.com";
-
-export const getDataFromDB = async (type) => {
-  const response = await fetch(`${baseURL}/${type}.json`);
-  const fetchedData = await response.json();
-  return fetchedData;
-};
-
-export const getDataFromDBasList = async (type, isTotal) => {
-  const pushFetchedDataToList = (data) => {
-    const list = [];
-    if (data) {
-      isTotal
-        ? Object.keys(data).map((key) => {
-            return list.push({
-              [key]: data[key],
-              id: key,
-            });
-          })
-        : Object.keys(data).map((key) => {
-            return list.push({
-              ...data[key],
-              id: key,
-              type: type,
-            });
-          });
-    }
-    return list;
-  };
-
-  const fetchedData = await getDataFromDB(type);
-  const fetchedDataList = pushFetchedDataToList(fetchedData);
-  return fetchedDataList;
-};
-
-const URL = "http://localhost:8080/";
+const baseURL = "http://localhost:8080/";
 
 export const getData = async (type) => {
   try {
-    const response = await fetch(`${URL}${type}`, {
+    const response = await fetch(`${baseURL}${type}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -53,7 +18,7 @@ export const getData = async (type) => {
 
 export const postData = async (type, form) => {
   try {
-    const response = await fetch(`${URL}${type}`, {
+    const response = await fetch(`${baseURL}${type}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -70,7 +35,7 @@ export const postData = async (type, form) => {
 
 export const deleteTransaction = async (type, transaction) => {
   try {
-    const response = await fetch(`${URL}${type}`, {
+    const response = await fetch(`${baseURL}${type}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -91,7 +56,7 @@ export const updateTransaction = async (
   newTransaction
 ) => {
   try {
-    const response = await fetch(`${URL}${type}`, {
+    const response = await fetch(`${baseURL}${type}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
